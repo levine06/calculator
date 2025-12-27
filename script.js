14c40c32f361acdc22f412b050d0e98c3bab37ee
levine <levine.chin19@gmail.com>
2025-12-27 14:41:08 +0800

    implement clear and allclear

--- script.js
+++ script.js
@@ -4,22 +4,40 @@ buttons.forEach((button) => {
 });
 
 const display = document.querySelector("#display");
-let displayText = "";
+let firstOperand = null;
+let operator = null;
+let shouldReset = false; // The next number click should reset display
 
 function calculator(e) {
     // If a number is clicked
     if (e.target.classList.contains("number")) {
-        numberClicked = e.target.innerText
+        if (shouldReset) {
+            display.innerText = "";
+            shouldReset = false;
+        }
+        const numberClicked = e.target.innerText;
         display.innerText += numberClicked;
     }
 
     // If an operator (% . + - * / =) is clicked
     else if (e.target.classList.contains("operator")) {
-
+        operatorClicked = e.target.datset.op;
+        performOperation(operatorClicked);
     }
 
     // If C or AC is clicked
     else {
-
+        if (e.target.id === "all-clear") {
+            display.innerText = "";
+        }
+        else if (e.target.id === "clear") {
+            if (display.innerText !== "") {
+                display.innerText = display.innerText.slice(0, -1);
+            }
+        }
     }
+}
+
+function performOperation(operator) {
+
 }
